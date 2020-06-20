e63a28935cf1dfa13b720c7948cc07fe25c262d7
nicole <nicolef@post.bgu.ac.il>
2020-06-20 17:52:12 +0300

    .

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,31 +1,68 @@
 <template>
   <div class="container">
-    <h1 class="title">Main Page</h1>
-    <RecipePreviewList title="Randome Recipes" class="RandomRecipes center" />
-    <router-link v-if="!$root.store.username" to="/login" tag="button">You need to Login to vue this</router-link>
-    {{ !$root.store.username }}
-    <RecipePreviewList
-      title="Last Viewed Recipes"
-      :class="{
-        RandomRecipes: true,
-        blur: !$root.store.username,
-        center: true
-      }"
-      disabled
-    ></RecipePreviewList>
-    <!-- <div
-      style="position: absolute;top: 70%;left: 50%;transform: translate(-50%, -50%);"
-    >
-      Centeredasdasdad
-    </div>-->
+   <!-- <h1 class="title">Main Page</h1>-->
+    <b-container class="bv-example-row">
+      <b-row >
+        <b-col >
+          <RecipePreviewList title="Explore these Recipes" class="RandomRecipes" />
+          <br>
+          <div class="new">
+            <button @click="updateRecipes">New Recipes</button> <!--  עדיין לא עובד כמו שצריך-->
+          </div>
+        </b-col>
+        <b-col>
+          <div v-if="!$root.store.username" to="/login" >
+            <Login />
+           </div>
+           <div v-else >
+             <RecipePreviewList
+            title="Last Watched Recipes"
+            :class="{
+              RandomRecipes: true,
+              blur: !$root.store.username,
+              center: true,
+            }"
+            disabled
+          > </RecipePreviewList>
+          </div>
+         
+        </b-col>
+      </b-row>
+    </b-container>
+   
   </div>
 </template>
 
 <script>
 import RecipePreviewList from "../components/RecipePreviewList";
+import RecipePreview from "../components/RecipePreview.vue";
+import Login from "../pages/LoginPage";
 export default {
   components: {
-    RecipePreviewList
+    RecipePreviewList,
+   
+    Login,
+  
+  },
+   mounted() {
+    this.updateRecipes();
+  },
+  methods: {
+    async updateRecipes() {
+      try {
+        const response = await this.axios.get(
+          "https://test-for-3-2.herokuapp.com/recipes/random"
+        );
+
+        // console.log(response);
+        const recipes = response.data.recipes;
+        this.recipes = [];
+        this.recipes.push(...recipes);
+        // console.log(this.recipes);
+      } catch (error) {
+        console.log(error);
+      }
+    }
   }
 };
 </script>
@@ -33,6 +70,11 @@ export default {
 <style lang="scss" scoped>
 .RandomRecipes {
   margin: 10px 0 10px;
+  text-align: center;
+}
+.new {
+  margin: 10px 0 10px;
+  text-align: center;
 }
 .blur {
   -webkit-filter: blur(5px); /* Safari 6.0 - 9.0 */
@@ -42,4 +84,5 @@ export default {
   pointer-events: none;
   cursor: default;
 }
+
 </style>
