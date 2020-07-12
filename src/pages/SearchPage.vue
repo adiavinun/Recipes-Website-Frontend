94eb46e7c19e93d73e22a739452ccc6175d0c595
adiavinun <49281852+adiavinun@users.noreply.github.com>
2020-07-13 00:21:14 +0300

    .

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="container">
-    <h1 class="title"><b-icon icon="search"></b-icon>   Search Page</h1>
-    <h2>What's your desire? <b-icon icon="chat-dots"></b-icon> </h2>
+    <h1 class="title"><b-icon icon="search"></b-icon>   Search</h1>
+    <h4>What's your desire? <b-icon icon="chat-dots"></b-icon> </h4>
     
     <div style="display: flex;">
        <b-form >
       <b-form-input id="search" v-model="searchContent" placeholder="The name of the recipe or dish you would like to look for" type="search" list="search-options-list" style="width:400px; padding: 5px;" ></b-form-input>
+      <br>
       <b-form-group id="input-group-cuisines" label-cols-sm="3" label="cuisines:" label-for="cuisines">
         <b-form-select id="cuisines" v-model="cuisinesInput" name="cuisines" placeholder="Select by cuisine" :value="null">
           <option disabled value="">Select by cuisine</option>
@@ -15,20 +16,21 @@
       </b-form-group>
       <b-form-group id="input-group-diets" label-cols-sm="3" label="diets:" label-for="diets">
         <b-form-select id="diets" v-model="dietsInput" name="diets">
-          <option disabled value="">Select by Diet</option>
+          <option disabled value="">Select by diet</option>
         <!--<option disabled selected value>Please Select Diet</option>-->
         <option v-for="(diet, index) in diets" :value="diet" :key="index">{{diet}}</option>
         </b-form-select>
       </b-form-group>
       <b-form-group id="input-group-intolerances" label-cols-sm="3" label="intolerances:" label-for="intolerances">
         <b-form-select id="intolerances" v-model="intolerancesInput" name="intolerances">
-          <option disabled value="">Select by Intolerance</option>
+          <option disabled value="">Select by intolerance</option>
         <!--<option disabled selected value>Please Select Intolerance</option>-->
         <option v-for="(intolerance, index) in intolerances" :value="intolerance" :key="index">{{intolerance}}</option>
         </b-form-select>
       </b-form-group>
       <b-button type="reset" variant="danger">Reset</b-button>
-      <b-button type="button" class="btn btn-primary" tyle="width:250px;" @click="search()" variant="primary">Search</b-button>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <b-button type="button" class="btn btn-primary" style="width:330px;" @click="search()" variant="primary">Search</b-button>
 
      </b-form >
     </div>
