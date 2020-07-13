5132d6f3049a392ab23c11dcfa999ad02f38a57a
nicole <nicolef@post.bgu.ac.il>
2020-07-13 20:51:37 +0300

    .

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -4,15 +4,14 @@
     <h4>What's your desire? <b-icon icon="chat-dots"></b-icon> </h4>
     
     <div style="display: flex;">
-       <b-form >
-      <b-form-input id="search" v-model="searchContent" placeholder="The name of the recipe or dish you would like to look for" type="search" list="search-options-list" style="width:400px; padding: 5px;" ></b-form-input>
+       <b-form @reset.prevent="onReset">
+      <b-form-input id="search" v-model="searchContent" placeholder="The name of the recipe or dish you would like to look for" type="search" list="search-options-list" style="width:430px; padding: 5px;" ></b-form-input>
       <b-form-group label="Number of search results:">
       <b-form-radio-group id="radio-group-1" v-model="selected" :options="options" name="radio-options"></b-form-radio-group>
     </b-form-group>
       <b-form-group id="input-group-cuisines" label-cols-sm="3" label="cuisines:" label-for="cuisines">
         <b-form-select id="cuisines" v-model="cuisinesInput" name="cuisines" placeholder="Select by cuisine" :value="null">
           <option disabled value="">--Select by cuisine--</option>
-        <!--<option disabled selected value>Please Select Cuisine</option>-->
         <option v-for="(cuisine, index) in cuisines" :value="cuisine" :key="index">{{cuisine}}</option>
         </b-form-select>
       </b-form-group>
@@ -30,11 +29,11 @@
         <option v-for="(intolerance, index) in intolerances" :value="intolerance" :key="index">{{intolerance}}</option>
         </b-form-select>
       </b-form-group>
-      <b-button type="reset" variant="danger">Reset</b-button>
+    <!--  <b-button type="reset" variant="danger" @click="onReset()">Reset</b-button>-->
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       <!--<b-button type="button" class="btn btn-primary" tyle="width:250px;" @click="search()" variant="primary">Search</b-button>-->
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <b-button type="button" class="btn btn-primary" style="width:330px;" @click="search()" variant="primary" :disabled="!searchContent.length">Search</b-button>
+      <b-button type="button" class="btn btn-primary" style="width:430px;" @click="search()" variant="primary" :disabled="!searchContent.length">Search</b-button>
        <br><br><br>
        <b-form-group id="input-group-sort" label-cols-sm="3" label="Sort By:" label-for="Sort By">    
           <b-form-select v-model="sort" @change="sortby" :disabled="!recipes || !recipes.length" width="20px">
@@ -46,14 +45,20 @@
           </b-form-select>
       </b-form-group>
      
-      <b-form-group id="input-group-intolerances" label-cols-sm="3" label-for="Your last search was"> 
-      <br>  <h4>The result search:</h4>
+      <b-form-group v-if="recipes.length" id="input-group-intolerances" label-cols-sm="3" label-for="Your last search was" > 
+      <br>       
+       <h4 >The result search:</h4>
+      
         <div v-for="r in recipes" :key="r.id"> 
         <RecipePreview title="The result search:" class="recipePreview" :recipe="r" />
         <br>
         </div>
       </b-form-group>
      <!--<h4>Your last search was:</h4>-->
+    
+      <b-form-group v-if="noResults" class="empty">
+        <h4> No results returned</h4>
+      </b-form-group>
      </b-form >
      <br>
 
@@ -76,6 +81,7 @@ export default {
   data() {
     return {
       cuisines: [
+        "",
         "African",
         "American",
         "British",
@@ -104,6 +110,7 @@ export default {
         "Vietnamese"
       ],
       diets: [
+        "",
         "Gluten Free",
         "Ketogenic",
         "Vegetarian",
@@ -116,6 +123,7 @@ export default {
         "Whole30"
       ],
       intolerances: [
+        "",
         "Dairy",
         "Egg",
         "Gluten",
@@ -133,6 +141,7 @@ export default {
       cuisinesInput: "",
       dietsInput: "",
       intolerancesInput: "",
+      noResults: false,
       recipes: [],
       sort: null,
       selected: '5',
@@ -169,6 +178,9 @@ export default {
          console.log(response);
         this.recipes = [];
         this.recipes.push(...searchRecipes);
+        if (this.recipes.length == 0) {
+          this.noResults = true;
+        }
       } catch (err) {
         console.log(err.response);
         //this.form.submitError = err.response.data.message;
@@ -206,8 +218,26 @@ export default {
       } 
       console.log("changed");
       console.log(this.num_of_recipes);
-    },  
-  
+    }, 
+    onReset() {
+      this.form = {
+        cuisines: "",
+        diets: "",
+        intolerances: "",
+        searchContent: "",
+        cuisinesInput: "",
+        dietsInput: "",
+        intolerancesInput: "",
+        noResults: false,
+        recipes: [],
+        sort: null,
+        selected: '5',
+      };
+       this.$nextTick(() => {
+        this.$reset();
+      });
+    }
+    
   }
 };
 </script>
@@ -216,4 +246,7 @@ export default {
 .container {
   max-width: 500px;
 }
+ .empty{
+   color: red;
+ }
 </style>
